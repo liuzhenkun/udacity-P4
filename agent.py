1899f39ef818cd756df50bcb6efb5db71a0f62ab
liuzhenkun <liuzk@lhcis.com>
2018-06-04 15:40:46 +0800

    fix some statements

--- agent.py
+++ agent.py
@@ -205,7 +205,7 @@ def run():
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05
     #   n_test     - discrete number of testing trials to perform, default is 0
     # the lower tolerance will make the more training times
-    sim.run(n_test=10, tolerance=0.01)
+    sim.run(n_test=10, tolerance=0.1)
 
 
 if __name__ == '__main__':
